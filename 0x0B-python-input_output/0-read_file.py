78f47c1475ed873bb77d7756e480e19b1e0f1fa8
Niamask <niamaskay@gmail.com>
2023-08-15 12:25:25 -0700

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Read text file function
+"""
+
+def read_file(filename=""):
+    """The read file function"""
+
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
