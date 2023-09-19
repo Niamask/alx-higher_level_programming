109375b45f49f7ab1891a4bc12fb88f2f9273bae
Niamask <niamaskay@gmail.com>
2023-09-19 09:23:45 -0700

    16. Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,6 @@
 -- lists all records of the table second_table of the database hbtn_0c_0;
+-- DELETE FROM second_table WHERE 	name = 'George';
+-- INSERT INTO second_table (id , name, score) VALUES (3, 'Aria', 18), (4, 'Aria', 12);
+-- SELECT score, name FROM second_table ORDER BY score DESC;
 
-DELETE FROM second_table WHERE 	name = 'George';
-INSERT INTO second_table (id , name, score) VALUES (3, 'Aria', 18), (4, 'Aria', 12);
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
