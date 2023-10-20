36ce567914914bf13aee4a51abfc46b2eeb52304
Niamask <niamaskay@gmail.com>
2023-10-20 05:00:24 -0700

    5. All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script takes in the name of a state as an argument and lists all cities
+of that state.
+database `hbtn_0e_4_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute("SELECT cities.name FROM cities JOIN states WHERE \
+            states.name = %(stateName)s and cities.state_id = states.id \
+            ORDER BY cities.id ASC", {'stateName' : argv[4]})
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
+    db_cursor.close()
+    db_connect.close()
