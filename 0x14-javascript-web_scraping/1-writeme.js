13e98fea9b042ea1ec1cc8e53265b2d41e8474ef
Niamask <niamaskay@gmail.com>
2023-11-28 04:45:30 -0800

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
-fs.writeFile(filePath, stringToWrite, (err) => {
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
     console.error(err);
     return;
